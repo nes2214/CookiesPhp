b6ae8c427b4dac0193a038aa3e3899bd6f21e26c
Néstor Cuevas Romero <ncu4303@proven.local>
2025-11-07 17:43:41 +0100

    Ejercicio formularios puesto navbar

--- formularios/405subidaImagen.php
+++ formularios/405subidaImagen.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">  
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php include 'navbar.php'; ?>
+</body>
+</html>
 <?php
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     if(isset($_POST['altura']) && isset($_POST['anchura'])){
